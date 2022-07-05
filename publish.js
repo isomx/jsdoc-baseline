82d9550262e79387328c2172e3462f325f585c3d
ugility <git@ugilityenterprises.com>
2022-07-05 11:55:22 -0400

    Updates to handle isomx integration, specifically the additional properties supported in jsdoc.config.json such as output hashes to break browser caching in production. Also converted data output files to json from js, and bumped version to match version of isomx packages.

--- publish.js
+++ publish.js
@@ -36,6 +36,47 @@ function init(filepaths) {
     LinkFormatter = finders.modules.require('./LinkFormatter');
 }
 
+/**
+ * Determines the hash to use for a destination path. This
+ * is used to ensure the output files aren't affected by
+ * browser caching in production mode.
+ *
+ * Typically, a hash is provided via process.env when running
+ * jsdoc (i.e., when using a Fork job from @isomx/ui), so this
+ * attempts to resolve it from there. If not,
+ * it falls back to determining the value to use based on
+ * the config file.
+ * @param {Object} obj - The jsdoc config file
+ * @param {string} envVar - The name of the var in process.env
+ * to attempt to use.
+ * @returns {?string}
+ * The hash, which will also be set in the config file.
+ * @ignore
+ */
+const resolveDestHash = (obj, envVar) => {
+    let hash = process.env[envVar];
+    if (typeof hash === 'string') {
+        obj.destinationHash = hash && hash !== 'null' ? hash : null;
+        return obj.destinationHash;
+    }
+    if (!(hash = obj.destinationHash)) {
+        obj.destinationHash = null;
+        return null;
+    }
+    if (hash !== true) {
+        obj.destinationHash = hash;
+        return hash;
+    }
+    try {
+        const pkg = require(path.join(process.cwd(), 'package.json'));
+        hash = pkg.version.replace(/\./g, '_');
+    } catch(e) {
+        throw new Error(`A destinationHash was provided, but it is \`true\`, indicating the package's version should be used. But no package.json was found in the current working directory.`);
+    }
+    obj.destinationHash = hash;
+    return hash;
+}
+
 exports.publish = (data, opts, tutorials) => {
     const conf = config.loadSync().get();
     let docletHelper;
@@ -46,17 +87,52 @@ exports.publish = (data, opts, tutorials) => {
 
     // load the core modules using the file finder
     init(conf.modules);
-
-
-    linkFormatter = new LinkFormatter(conf);
-    if (conf.privateSrcFilesMap) {
-        privateSrcFilesMap = require(path.join(process.cwd(), conf.privateSrcFilesMap));
-    } else {
+    /*
+     *
+     */
+    const hash = resolveDestHash(opts, 'DOCS_DESTINATION_HASH');
+    const dataOutput = opts.dataOutput;
+    if (dataOutput && dataOutput.destination) {
+        let { destination } = dataOutput;
+        const hash = resolveDestHash(dataOutput, 'DOCS_DATA_DESTINATION_HASH');
+        if (hash) {
+            destination = path.join(destination, hash);
+        }
+        if (!path.isAbsolute(destination)) {
+            destination = path.join(process.cwd(), destination);
+        }
+        dataOutput.destination = destination;
+        if (dataOutput.privateSrcFilesMap) {
+            privateSrcFilesMap = require(
+              path.join(destination, 'privateSrcFilesMap.json')
+            );
+        }
+    }
+    if (!privateSrcFilesMap) {
         privateSrcFilesMap = {};
     }
+    let { linksBasePath } = opts;
+    if (hash) {
+        linksBasePath = path.join(linksBasePath || '/', hash);
+    }
+    if (linksBasePath) {
+        linksBasePath = linksBasePath.replace(/\\/g, '/');
+        if (linksBasePath.charAt(0) !== '/') {
+            linksBasePath = `/${linksBasePath}`;
+        }
+        if (linksBasePath.charAt(linksBasePath.length - 1) !== '/') {
+            linksBasePath = `${linksBasePath}/`;
+        }
+    } else {
+        linksBasePath = null;
+    }
+    linkFormatter = new LinkFormatter(linksBasePath);
+    opts.privateSrcFilesMap = privateSrcFilesMap;
+    opts.linksBasePath = linksBasePath;
+    opts.linkFormatter = linkFormatter;
     docletHelper = new DocletHelper(linkFormatter);
-    template = new Template(conf, privateSrcFilesMap);
-    job = new PublishJob(template, opts, linkFormatter, privateSrcFilesMap);
+    template = new Template(conf, opts);
+    job = new PublishJob(template, opts);
 
     // set up tutorials
     helper.setTutorials(tutorials);
@@ -75,7 +151,6 @@ exports.publish = (data, opts, tutorials) => {
     // generate globals page if necessary
     job.generateGlobals(docletHelper.globals);
 
-    // console.log('ALL = ', docletHelper.all);
     // generate TOC data and index page
     job.generateTocData({
         hasGlobals: docletHelper.hasGlobals(),
@@ -103,4 +178,14 @@ exports.publish = (data, opts, tutorials) => {
         .copyStaticFiles();
 
     job.generateRawDataOutput(docletHelper);
+    /*
+     * See if we're being run via a fork job.
+     * If so, call complete, the script
+     * won't exit automatically.
+     */
+    if (global.devApi) {
+        setTimeout(() => {
+            global.devApi.scriptComplete(0);
+        }, 5);
+    }
 };
